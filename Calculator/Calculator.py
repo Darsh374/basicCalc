52c85014aa2ae75c9a94b53b99cb244cc9133b6c
Darsh374 <dp594@njit.edu>
2020-02-04 13:50:06 -0500

    added addition and subtraction

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,7 +1,21 @@
+from MathOperations.addition import Addition
+
+import math
+
+
 class Calculator:
 
     def __init__(self):
         pass
 
-    def addition(self, a, b):
-        return a + b
+    def Sum(self, a, b):
+        return Addition.sum(a, b):
+
+    def Subtraction(self, a, b):
+        return Subtraction.difference(a, b)
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a // b
--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -1,19 +1,30 @@
 import unittest
 
-from Calculator.Calculator import Calculator
+from MathOperations.addition import Addition
+from MathOperations.subtraction import Subtraction
 
 
 class MyTestCase(unittest.TestCase):
 
-    def test_instantiate_calculator(self):
+    def test_MathOperations_Addition(self):
         calculator = Calculator()
+        result = calcualtor.Sum(1, 2)
+        self.assertEqual(3, Addition.sum(1, 2))
 
-        self.assertIsInstance(calculator, Calculator)
-
-    def test_calculator_addition(self):
+    def test_calculator_subtraction(self):
         calculator = Calculator()
-        result = calculator.addition(1, 2)
-        self.assertEquals(3, result)
+        result = calcualtor.(1, 2)
+        self.assertEqual(-1, Subtraction.difference(1, 2))
+
+    def test_calculator_multiply(self):
+        calculator = Calculator()
+        result = calculator.multiply(1, 2)
+        self.assertEqual(2, result)
+
+    def test_calculator_divide(self):
+        calculator = Calculator()
+        result = calculator.divide(6, 3)
+        self.assertEqual(2, result)
 
 
 if __name__ == '__main__':
