eba5396a4bcd5da64a6263557726209ccceb7219
Darsh374 <dp594@njit.edu>
2020-02-04 13:55:35 -0500

    added addition and subtraction

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -5,13 +5,14 @@ import math
 
 class Calculator:
 
+    public result;
     def __init__(self):
         pass
 
     def Sum(self, a, b):
-        return Addition.sum(a, b):
+        return Addition.sum(a, b);
 
-    def Subtraction(self, a, b):
+    def Difference(self, a, b):
         return Subtraction.difference(a, b)
 
     def multiply(self, a, b):
