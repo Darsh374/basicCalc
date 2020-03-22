ceb657aeb36a44103536a52865251458f9adae8d
Darsh374 <dp594@njit.edu>
2020-03-22 13:26:18 -0400

    task: fixing calc

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,11 +1,10 @@
 from MathOperations.addition import Addition
 from MathOperations.subtraction import Subtraction
 from MathOperations.multiplication import Multiplication
+from MathOperations.division import Division
 from MathOperations.exponentiation import Exponentiation
-from MathOperations.log import Log
 from MathOperations.nthroot import Nthroot
-
-
+from MathOperations.log import Log
 
 class Calculator:
     Result = 0
@@ -14,31 +13,29 @@ class Calculator:
         pass
 
     def Sum(self, a, b):
-        self.Result = Addition.sum(a, b)
+        self.Result = Addition.sum(a,b)
         return self.Result
 
     def Difference(self, a, b):
-        self.Result = Subtraction.difference(a, b)
+        self.Result = Subtraction.difference(a,b)
         return self.Result
 
     def Product(self, a, b):
-        self.Result = Multiplication.product(a, b)
-        return self.Result
-
-    def Root(self, a, b):
-        self.Result = Nthroot.root(a, b)
-        return self.Result
-
-    def Power(self, a, b):
-        self.Result = Exponentiation.power(a, b)
-        return self.Result
-
-    def Logarithm(self, a, b):
-        self.Result = Log.logarithm(a, b)
+        self.Result = Multiplication.product(a,b)
         return self.Result
 
     def Quotient(self, a, b):
         self.Result = Division.quotient(a,b)
         return self.Result
 
+    def Root(self, a, b):
+        self.Result = Nthroot.root(a,b)
+        return self.Result
 
+    def Power(self, a, b):
+        self.Result = Exponentiation.power(a,b)
+        return self.Result
+
+    def Logarithm(self, a, b):
+        self.Result = Log.logarithm(a,b)
+        return self.Result
